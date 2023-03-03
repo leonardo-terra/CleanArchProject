5aaf2406214bb24e6e109321c2273b3a4750e646
leonardo-terra <leo.terra.melo@gmail.com>
2023-03-03 00:00:23 -0300

    dev: Implementa dominio Produto e Category e suas validações

---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Validation;
+
+namespace Domain.Entities
+{
+    public sealed class Category
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public ICollection<Product> Products { get; set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name");
+            DomainExceptionValidation.When(name.Length < 3, "Name should have at least 3 characters");
+            Name = name;
+        }
+
+        public void Update(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Validation;
+
+namespace Domain.Entities
+{
+    public sealed class Product
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+        
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Name must have at least 3 characters");
+            
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Description is required");
+            DomainExceptionValidation.When(description.Length < 10, "Description must have at least 3 characters");
+
+            DomainExceptionValidation.When(price < 0, "Invalid Price.");
+            
+            DomainExceptionValidation.When(stock < 0, "Invalid Stock.");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(image), "Image is required");
+            DomainExceptionValidation.When(image.Length < 10, "Description must have at least 10 characters");
+        }
+    }
+}
